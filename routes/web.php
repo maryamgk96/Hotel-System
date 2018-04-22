64ce5cb54955770e47364f05e2e36a9640e48c84
WessamSaeid <wessam.saeid33@gmail.com>
2018-04-22 22:58:07 +0200

    edit and delete floors

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('floors', 'FloorsController@index')->name('floors.index');
 Route::get('floordata', 'FloorsController@data');
 Route::get('floors/create', 'FloorsController@create');
 Route::post('floors', 'FloorsController@store');
+Route::get('floors/{id}/edit', 'FloorsController@edit');
+Route::put('floors/{id}', 'FloorsController@update');
+Route::delete('floors/{id}', 'FloorsController@destroy');
