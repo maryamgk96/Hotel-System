c4a927764fef05c89387bbc2561c00938ad29c82
maryam <maryam.h.gk@gmail.com>
2018-04-25 20:57:55 +0200

    fix bugs

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -6,20 +6,7 @@
         <!-- Sidebar Menu -->
         <ul class="sidebar-menu">
         
-@role('admin')
-            <li><a href="#"><span>Manage Mangers</span></a></li>
-    @role('manager')           
-            <li><a href="#"><span>Manage Receptionists</span></a></li>
-        @role('receptionist') 
-            <li><a href="#"><span>Manage Clients</span></a></li>
-            <li><a href="/floors"><span>Manage Floors</span></a></li>
-            <li><a href="/rooms"><span>Manage Rooms</span></a></li>
-            <li><a href="#"><span>My Approved Clients</span></a></li>
-            <li><a href="#"><span>Clients Reservations</span></a></li>
-            <li><a href="#"><span>Edit Profile</span></a></li>
-        @endrole
-    @endrole
-@endrole
+
             <li><a href="/reservations"><span>My Reservations</span></a></li>
             <li><a href="/reservations/rooms"><span>Make Reservation</span></a></li>
             <li><a href="#"><span>Register</span></a></li>
