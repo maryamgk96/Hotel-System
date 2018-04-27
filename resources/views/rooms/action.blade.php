b63493d049f7325aa9a819beb4edb45a5ac3c93b
WessamSaeid <wessam.saeid33@gmail.com>
2018-04-27 22:32:54 +0200

    delete floors and rooms with ajax

--- resources/views/rooms/action.blade.php
+++ resources/views/rooms/action.blade.php
@@ -1,7 +1,3 @@
-<form action="/rooms/{{$id}}" method="post" >
-    @csrf
-    @method('DELETE')
-    <a href="/rooms/{{$id}}/edit" class="btn btn-xm btn-primary" ><i class="fa fa-edit"></i> Edit</a>
-    <button class="btn btn-danger" onclick="return confirm('Are You Sure You Would Like to Delete This Room?');">Delete</button>       
-</form>
 
+<button  target="{{$id}}"  class='delete btn btn-xm btn-danger' > Delete </button>
+<a href="/rooms/{{$id}}/edit" class="btn btn-xm btn-primary" ><i class="fa fa-edit"></i> Edit</a>
